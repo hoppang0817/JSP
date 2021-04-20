fc65d96fe24b9b82ab6fb370840e4671458f6206
hoppang0817 <62546979+hoppang0817@users.noreply.github.com>
2021-04-20 16:48:21 +0900

    20200420

--- mvc_board/src/command/BReplyCommand.java
+++ mvc_board/src/command/BReplyCommand.java
@@ -3,11 +3,25 @@ package command;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.BDao;
+import dto.bDto;
+
 public class BReplyCommand implements BCommand {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
+		//request로 넘어온 값은 string으로 만 받을 수있음
+		String bId =request.getParameter("bId");
+		String bName =request.getParameter("bName");
+		String bTitle =request.getParameter("bTitle");
+		String bContent =request.getParameter("bContent");
+		String bGroup =request.getParameter("bGroup");
+		String bStep =request.getParameter("bStep");
+		String bIndent =request.getParameter("bIndent");
+		
+		
+		BDao dao = new BDao();
+		dao.reply(bId, bName, bTitle, bContent, bGroup, bStep, bIndent);
 
 	}
 
